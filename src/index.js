30eadca01977d773aad6670538aff08fa142cfd2
Zapihra <iida.vaaja@student.lut.fi>
2023-11-05 19:36:15 +0200

    week 1 tasks

--- src/index.js
+++ src/index.js
@@ -4,10 +4,7 @@ document.body.onload = main;
 
 function addElement(breed) {
 
-    const body = document.getElementById("body");
-
-    const divCon = document.createElement("div");
-    divCon.className = "container";
+    const divCon = document.getElementById("container");
 
     const divItem = document.createElement("div");
     divItem.className = "wiki-item";
@@ -21,40 +18,73 @@ function addElement(breed) {
 
     const p = document.createElement("p");
     p.className = "wiki-text";
-    p.textContent = "text of breed";
 
     divContent.appendChild(p);
 
     divItem.appendChild(h1);
     divItem.appendChild(divContent);
 
+    divItem.appendChild(divContent);
+
+    divCon.appendChild(divItem);
+
+}
+
+function addPicture(website, i) {
+
+    const divCon = document.getElementsByClassName("wiki-content")
+
+    const get = "https://dog.ceo/api/breed/" + website + "/images/random";
+
     const divImgCon = document.createElement("div");
     divImgCon.className = "img-container";
 
     const divImg = document.createElement("img");
     divImg.className = "wiki-img";
-    divImg.src = "";
+
+    fetch(get).then(function (response) {return response.json();
+    }).then(function (data) {
+        divImg.src = data.message;
+    })
 
     divImgCon.appendChild(divImg);
 
-    divContent.appendChild(divImgCon);
+    divCon[i].appendChild(divImgCon);
 
-    divItem.appendChild(divContent);
+}
 
-    divCon.appendChild(divItem);
+function addWiki(breed, i) {
+    const get = "https://en.wikipedia.org/api/rest_v1/page/summary/" + breed;
 
-    body.appendChild(divCon);
+    const wikiText = document.getElementsByClassName("wiki-text")
 
+    fetch(get).then(function (response) {return response.json();
+    }).then(function (data) {        
+        wikiText[i].textContent = data.extract;
+    })
 
 }
 
 function main() {
-    const breeds = ["border terrier", "buhund", "shetland Sheepdog", "shiba", "whippet"]
+    const breeds = ["Border Terrier", "Buhund", "Shetland Sheepdog", "Shiba", "Whippet"]
+    const website = ["terrier/border", "buhund", "sheepdog/shetland", "shiba", "whippet"]
+    const wiki = ["border_terrier", "buhund", "shetland_sheepdog", "shiba", "whippet"]
+
+    const divCon = document.createElement("div");
+    divCon.className = "container";
+    divCon.id = "container";
+
+    body.appendChild(divCon);
 
     for (let i = 0; i < 5; i++) {
         addElement(breeds[i]);
+        addPicture(website[i], i);
+        addWiki(wiki[i], i);
         
     }
+
+    
+    
 }
 
 
